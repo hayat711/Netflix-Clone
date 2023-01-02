0a0910a77e56b6231d7165db72a9ecda22568739
hayat711 <71981169+hayat711@users.noreply.github.com>
2023-01-02 20:34:49 +0900

    Update firebase.prod.js

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -2,7 +2,7 @@ import Firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-// 1) when seeding the database you'll have to uncomment this!
+
 
 const config = {
       apiKey: process.env.FIREBASE_API_KEY,
@@ -14,8 +14,6 @@ const config = {
 };
 
 const firebase = Firebase.initializeApp(config);
-// 2) when seeding the database you'll have to uncomment this!
 
-// 3) once you have populated the database (only run once!), re-comment this so you don't get duplicate data
 
 export { firebase };
